8ce94ea3fc857ed9b9702bfc9b871df0bacc4290
melody605 <melodyvalentine9@gmail.com>
2024-12-05 14:24:21 -0500

    apiupdates

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,29 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
+
   // TODO: make a POST request to the login route
+  try { 
+    const response = await fetch('/auth/login', { 
+      method: 'POST',
+      headers: { 
+        'Content-Type': 'application/json' }, 
+      body: JSON.stringify(userInfo) 
+    });
+    
+    // che3cking if the response is correct, if not throws an error 
+
+    if (!response.ok) { throw new Error('Login request failed'); } 
+    const data = await response.json(); 
+    return data; 
+  } catch (error) { 
+    
+    console.log('Error from user login: ', error);  
+    return Promise.reject('Could not fetch user info');  
+  }
 }
 
 
-
 export { login };
+
+
